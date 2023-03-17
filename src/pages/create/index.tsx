7fb03b1481f8f023e8381a3a584bb54d2b5339fa
Dani Setien <dani.setien@gmail.com>
2023-03-17 15:48:17 +0100

    Add CreateFormPageStyled

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,7 +1,18 @@
+import { ToastContainer } from "react-toastify";
 import CreateForm from "../../components/CreateForm/CreateForm";
+import Header from "../../components/Header/Header";
+import CreateFormPageStyled from "../../styles/pages/CreateFormPageStyled";
 
 const CreatePage = () => {
-  return <CreateForm />;
+  return (
+    <>
+      <ToastContainer />
+      <CreateFormPageStyled>
+        <Header />
+        <CreateForm />
+      </CreateFormPageStyled>
+    </>
+  );
 };
 
 export default CreatePage;
