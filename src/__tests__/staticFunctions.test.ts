85b7ae84d9ffaf464f8df33d02746eafc0536d0e
Dani Setien <dani.setien@gmail.com>
2023-03-20 15:36:55 +0100

    Test getStaticPaths for DetailPage

--- src/__tests__/staticFunctions.test.ts
+++ src/__tests__/staticFunctions.test.ts
@@ -1,37 +1,42 @@
 import { GetStaticPropsContext, PreviewData } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { getStaticProps } from "../pages";
-import { getStaticProps as getStaticPropsForDetail } from "../pages/paintings/[id]";
+import {
+  getStaticPaths,
+  getStaticProps as getStaticPropsForDetail,
+} from "../pages/paintings/[id]";
 
 import { mockPaintings } from "../utils/testUtils/mockHardcodedData";
 
 jest.mock("../utils/functionsUtils/functionUtils", () => ({
   __esModule: true,
-  getPaintingsData: jest.fn().mockReturnValue({
-    id: "1",
-    author: "Mary Heilmann",
-    name: "New Line Up",
-    year: "2018",
-    gallery: "Private collection",
-    technique: "Oil on canvas",
-    size: "40 x 50 in",
-    medium: "Painting",
-    materials: "Oil paint, canvas",
-    unique: "true",
-    certificate: "true",
-    rarity: "unique",
-    condition: "excellent",
-    signature: "true",
-    price: "28000",
-    frame: "false",
-    highlightOrder: "1",
-    summary:
-      "Colorful abstract painting with horizontal lines and curved shapes",
-    image:
-      "https://icqwpkxwddqofeibjqcj.supabase.co/storage/v1/object/public/paitings/newLineUp.png?t=2023-03-12T13%3A23%3A46.999Z",
-    width: "300",
-    height: "400",
-  }),
+  getPaintingsData: jest.fn().mockReturnValue([
+    {
+      id: "1",
+      author: "Mary Heilmann",
+      name: "New Line Up",
+      year: "2018",
+      gallery: "Private collection",
+      technique: "Oil on canvas",
+      size: "40 x 50 in",
+      medium: "Painting",
+      materials: "Oil paint, canvas",
+      unique: "true",
+      certificate: "true",
+      rarity: "unique",
+      condition: "excellent",
+      signature: "true",
+      price: "28000",
+      frame: "false",
+      highlightOrder: "1",
+      summary:
+        "Colorful abstract painting with horizontal lines and curved shapes",
+      image:
+        "https://icqwpkxwddqofeibjqcj.supabase.co/storage/v1/object/public/paitings/newLineUp.png?t=2023-03-12T13%3A23%3A46.999Z",
+      width: "300",
+      height: "400",
+    },
+  ]),
   getDetailData: jest.fn().mockReturnValue({
     id: "1",
     author: "Mary Heilmann",
@@ -63,7 +68,7 @@ describe("Given a getStaticProps function that fetches a list of paintings", () 
   describe("When called", () => {
     test("Then it should return a props object with a list of paintings", async () => {
       const expectedProps = {
-        props: { paintingsData: mockPaintings[0] },
+        props: { paintingsData: [mockPaintings[0]] },
         revalidate: 1,
       };
       const context: GetStaticPropsContext<ParsedUrlQuery, PreviewData> = {};
@@ -90,6 +95,23 @@ describe("Given a getStaticProps function that fetches a particular painting", (
       const props = await getStaticPropsForDetail(context);
 
       expect(props).toStrictEqual(expectedProps);
+    });
+  });
+});
+
+describe("Given a getStaticPaths function that fetches the paths for a list of paintings", () => {
+  describe("When called", () => {
+    test("Then it should return a props object with the paths for that list of paintings", async () => {
+      const expectedPaths = {
+        paths: [{ params: { id: mockPaintings[0].id } }],
+        fallback: false,
+      };
+
+      const context: GetStaticPropsContext<ParsedUrlQuery, PreviewData> = {};
+
+      const paths = await getStaticPaths(context);
+
+      expect(paths).toStrictEqual(expectedPaths);
     });
   });
 });
